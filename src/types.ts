6cb3f831c0bf944027fabde938bfd5dad3096bed
Dusan Jovanov <jovanovdusan1@gmail.com>
2025-11-27 00:54:57 +0100

    IconPath now extends Path class from @neptune3d/path

--- src/types.ts
+++ src/types.ts
@@ -1,54 +1,12 @@
+import type {
+  CloseCommand,
+  CubicCommand,
+  LineCommand,
+  MoveCommand,
+  QuadCommand,
+} from "@neptune3d/path";
 import type { IconGlyph } from "./IconGlyph";
 import type { IconPath } from "./IconPath";
-
-export type MoveCommand = { type: "M"; x: number; y: number };
-
-export type LineCommand = { type: "L"; x: number; y: number };
-
-export type HCommand = { type: "H"; x: number };
-
-export type VCommand = { type: "V"; y: number };
-
-export type QuadCommand = {
-  type: "Q";
-  x1: number;
-  y1: number;
-  x: number;
-  y: number;
-};
-
-export type CubicCommand = {
-  type: "C";
-  x1: number;
-  y1: number;
-  x2: number;
-  y2: number;
-  x: number;
-  y: number;
-};
-
-export type SmoothQuadCommand = { type: "T"; x: number; y: number };
-
-export type SmoothCubicCommand = {
-  type: "S";
-  x2: number;
-  y2: number;
-  x: number;
-  y: number;
-};
-
-export type CloseCommand = { type: "Z" };
-
-export type IconPathCommand =
-  | MoveCommand
-  | LineCommand
-  | HCommand
-  | VCommand
-  | QuadCommand
-  | CubicCommand
-  | SmoothQuadCommand
-  | SmoothCubicCommand
-  | CloseCommand;
 
 export type OpenTypeCommand =
   | MoveCommand
@@ -56,35 +14,6 @@ export type OpenTypeCommand =
   | QuadCommand
   | CubicCommand
   | CloseCommand;
-
-export type Corner =
-  | { kind: "sharp" }
-  | { kind: "rounded"; rx: number; ry: number }
-  | { kind: "chamfer"; rx: number; ry: number };
-
-export type IconPathBounds = {
-  minX: number;
-  minY: number;
-  maxX: number;
-  maxY: number;
-};
-
-export type Point = { x: number; y: number };
-
-export type Rect = {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  rx?: number;
-  ry?: number;
-  corners?: {
-    tl?: Corner;
-    tr?: Corner;
-    br?: Corner;
-    bl?: Corner;
-  };
-};
 
 /**
  * Options for constructing an IconFont.
